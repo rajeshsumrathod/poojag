552fdf37ed31a05d18debccd48ecb1e95086e40e
rajeshsumrathod <rajeshsumrathod@gmail.com>
2020-08-18 09:26:18 +0530

    first commit

---
+++ rathod.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY World</title>
+</head>
+<body>
+    <?php
+    echo "Hello Pooja"."<br>";
+    echo "Rajesh Loves U"
+    ?>
+    
+</body>
+</html>
